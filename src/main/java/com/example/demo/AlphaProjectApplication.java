3ef6de6b2a20082c74299a8cc4854cc930373bf2
maxencelebec <maxence.lbc@gmail.com>
2018-12-18 14:56:23 +0100

    espace

--- src/main/java/com/example/demo/AlphaProjectApplication.java
+++ src/main/java/com/example/demo/AlphaProjectApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AlphaProjectApplication {
 
-	
 	public static void main(String[] args) {
 		SpringApplication.run(AlphaProjectApplication.class, args);
 	}

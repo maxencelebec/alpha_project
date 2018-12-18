ee1a3b594f936f7613c9193e537cc7cd9981ed27
maxencelebec <maxence.lbc@gmail.com>
2018-12-18 16:28:41 +0100

    redirection

---
+++ src/main/java/com/example/demo/controller/Home.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.demo.dao.ConnexionInscriptionDao;
+
+@Controller
+public class Home {
+
+	@Autowired
+	private ConnexionInscriptionDao connexionInscriptionDao;
+	
+	@RequestMapping(value="/parameters", method=RequestMethod.GET)
+	public String redirectParameters() {
+		return "parameters";
+	}
+	
+	@RequestMapping(value="/home", method=RequestMethod.GET)
+	public String redirectHome() {
+		return "home";
+	}
+	
+	@RequestMapping(value="/connexion", method=RequestMethod.GET)
+	public String redirectConnexion() {
+		return "index";
+	}
+}

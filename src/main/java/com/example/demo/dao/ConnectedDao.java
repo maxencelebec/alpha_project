c4d1a4afefb0830588ac4c6ea015943224db1d2d
BastienGalmiche <bastien.galmiche@yahoo.fr>
2018-12-18 17:41:01 +0100

    test

---
+++ src/main/java/com/example/demo/dao/ConnectedDao.java
@@ -0,0 +1,24 @@
+package com.example.demo.dao;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.example.demo.model.User;
+
+public interface ConnectedDao extends CrudRepository<User,Integer>{
+	
+	@Query("SELECT username FROM User where id=:id")
+	List<User> takeBdd(@Param ("id") int id);
+	
+	
+	@Modifying
+	@Query(value ="UPDATE user SET mail = :mail, username = :value, password = :password WHERE id = :id )")
+	@Transactional
+	void updateUser(@Param ("id") int id, @Param ("mail") String string, @Param ("password") String string2, @Param ("username") String string3);
+}
